9df80217bc35149887bc9f27c6aa00843a60db34
AsZaychik <aszaychik@gmail.com>
2023-11-08 17:30:02 +0700

    refactor(validation): fix indentation

--- internal/pkg/validation/validation_error.go
+++ internal/pkg/validation/validation_error.go
@@ -11,27 +11,27 @@ import (
 
 func getValidationErrorMessage(field, tag, param string) string {
 	switch tag {
-	case "required":
-		return fmt.Sprintf("%s is required", field)
-	case "min":
-		return fmt.Sprintf("%s must be at least %s", field, param)
-	case "max":
-		return fmt.Sprintf("%s must be at most %s", field, param)
-	case "email":
-		return fmt.Sprintf("%s must be a valid email address", field)
-	case "url":
-		return fmt.Sprintf("%s must be a valid URL", field)
-	case "alpha":
-		return fmt.Sprintf("%s must contain only alphabetical characters", field)
-	case "alphanum":
-		return fmt.Sprintf("%s must contain only alphanumeric characters", field)
-	case "numeric":
-		return fmt.Sprintf("%s must be a valid numeric value", field)
-	case "len":
-		return fmt.Sprintf("%s must be exactly %s characters long", field, param)
-	// Add more cases for other validation tags as needed
-	default:
-		return fmt.Sprintf("Validation error on field %s, tag %s", field, tag)
+		case "required":
+			return fmt.Sprintf("%s is required", field)
+		case "min":
+			return fmt.Sprintf("%s must be at least %s", field, param)
+		case "max":
+			return fmt.Sprintf("%s must be at most %s", field, param)
+		case "email":
+			return fmt.Sprintf("%s must be a valid email address", field)
+		case "url":
+			return fmt.Sprintf("%s must be a valid URL", field)
+		case "alpha":
+			return fmt.Sprintf("%s must contain only alphabetical characters", field)
+		case "alphanum":
+			return fmt.Sprintf("%s must contain only alphanumeric characters", field)
+		case "numeric":
+			return fmt.Sprintf("%s must be a valid numeric value", field)
+		case "len":
+			return fmt.Sprintf("%s must be exactly %s characters long", field, param)
+		// Add more cases for other validation tags as needed
+		default:
+			return fmt.Sprintf("Validation error on field %s, tag %s", field, tag)
 	}
 }
 
