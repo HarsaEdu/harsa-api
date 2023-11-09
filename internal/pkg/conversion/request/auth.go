2e2e3fcd16d61122a27f4a6b98a880d4ca4c42fe
madya santosa <devmadya@gmail.com>
2023-11-09 15:19:56 +0800

    feat(auth): add convertion for auth

--- internal/pkg/conversion/request/auth.go
+++ internal/pkg/conversion/request/auth.go
@@ -1,1 +1,16 @@
 package conversion
+
+import (
+	"github.com/HarsaEdu/harsa-api/internal/model/domain"
+	"github.com/HarsaEdu/harsa-api/internal/model/web"
+)
+
+func RegisterUserRequestToUserModel(r web.RegisterUserRequest) *domain.User {
+	return &domain.User{
+		Username:          r.Username,
+		Email:             r.Email,
+		Password:          r.Password,
+		RoleID:            r.RoleID,
+		RegistrationToken: r.RegistrationToken,
+	}
+}
--- internal/pkg/conversion/response/auth.go
+++ internal/pkg/conversion/response/auth.go
@@ -1,1 +1,15 @@
 package conversion
+
+import (
+	"github.com/HarsaEdu/harsa-api/internal/model/domain"
+	"github.com/HarsaEdu/harsa-api/internal/model/web"
+)
+
+func ConvertToAuthResponse(r *domain.Auth) *web.AuthResponse {
+	return &web.AuthResponse{
+		ID:       r.ID,
+		Username: r.Username,
+		Email:    r.Email,
+		RoleName: web.Role(r.RoleName),
+	}
+}
