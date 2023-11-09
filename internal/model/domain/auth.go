4c84968b91ca896490ae26b6020cb97f311cc045
madya santosa <devmadya@gmail.com>
2023-11-09 15:23:54 +0800

    feat(domain): add domain auth, user, and role

--- internal/model/domain/auth.go
+++ internal/model/domain/auth.go
@@ -1,1 +1,8 @@
 package domain
+
+type Auth struct {
+	ID       uint   `gorm:"type:int;primarykey" json:"id"`
+	Username string `gorm:"type:varchar(255)" json:"username"`
+	Email    string `gorm:"type:varchar(255)" json:"email"`
+	RoleName string `json:"role_name"`
+}
