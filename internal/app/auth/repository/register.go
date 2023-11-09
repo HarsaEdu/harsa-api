79c043334961b249cd4cc515fd1951d8d4ee82f6
madya santosa <devmadya@gmail.com>
2023-11-09 15:26:22 +0800

    feat(auth): add register repository

---
+++ internal/app/auth/repository/register.go
@@ -0,0 +1,31 @@
+package repository
+
+import (
+	"github.com/HarsaEdu/harsa-api/internal/model/domain"
+)
+
+func (r *AuthRepositoryImpl) RegisterUser(user *domain.User) (*domain.Auth, error) {
+	// define domain auth
+	auth := &domain.Auth{}
+
+	// insert user into database
+	result := r.DB.Create(&user)
+
+	// check if error when user inserted
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	// get user data from database
+	r.DB.Model(&domain.User{}).
+		Select("users.id as id, username, email, role.name as role_name").
+		Joins("left join roles on roles.id as role_id").
+		First(&auth)
+
+	// check if error when get user data from database
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return auth, nil
+}
