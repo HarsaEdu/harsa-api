d8c709c276caa447395e9afa8ef707a3e8fc6159
DimasAnugerahh <dimasanugrah883@gmail.com>
2023-11-13 23:36:26 +0800

    feat(handler): add handler of get category

---
+++ internal/app/categories/handler/category_handler_get_all.go
@@ -0,0 +1,26 @@
+package handler
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/HarsaEdu/harsa-api/internal/pkg/res"
+	"github.com/HarsaEdu/harsa-api/internal/pkg/validation"
+	"github.com/labstack/echo/v4"
+)
+
+func (categoryHandler *CategoryHandlereImpl) GetAll(ctx echo.Context) error {
+	response, err := categoryHandler.CategoryService.GetAll(ctx)
+	if err != nil {
+		if strings.Contains(err.Error(), "validation") {
+			return validation.ValidationError(ctx, err)
+		}
+		if strings.Contains(err.Error(), "not found") {
+			return res.StatusNotFound(ctx, "categoriy not found", err)
+		}
+		return res.StatusInternalServerError(ctx, "failed to create category, something happen", fmt.Errorf("internal server error"))
+	}
+
+	return res.StatusOK(ctx, "success get categories", response)
+
+}
