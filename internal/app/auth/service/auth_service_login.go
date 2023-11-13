949869c29371e676b2cf5f158e38d881de574020
madya santosa <devmadya@gmail.com>
2023-11-13 15:53:25 +0800

    feat(auth): add login service

---
+++ internal/app/auth/service/auth_service_login.go
@@ -0,0 +1,36 @@
+package service
+
+import (
+	"fmt"
+
+	"github.com/HarsaEdu/harsa-api/internal/model/web"
+	conversionResponse "github.com/HarsaEdu/harsa-api/internal/pkg/conversion/response"
+	"github.com/HarsaEdu/harsa-api/internal/pkg/password"
+	"github.com/labstack/echo/v4"
+)
+
+func (authService *AuthServiceImpl) LoginUser(ctx echo.Context, loginUser web.LoginUserRequest) (*web.AuthResponse, error) {
+	// check available username and email
+	existingUser, _ := authService.UserRepository.UserAvailable("", loginUser.Email)
+
+	if existingUser == nil {
+		return nil, fmt.Errorf("email or password is incorrect")
+	}
+
+	err := password.ComparePassword(existingUser.Password, loginUser.Password)
+
+	if err != nil {
+		return nil, fmt.Errorf("email or password is incorrect")
+	}
+
+	response, err := authService.AuthRepository.LoginUser(existingUser.ID)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to get data user")
+	}
+
+	// convert user data to auth response
+	userResponse := conversionResponse.ConvertToAuthResponse(response)
+
+	return userResponse, nil
+}
