51df146901fe4417278fc9d7cc78ddd9403aee9c
DimasAnugerahh <dimasanugrah883@gmail.com>
2023-11-13 23:35:50 +0800

    feat(handler): add handler of create category

---
+++ internal/app/categories/handler/category_handler_create.go
@@ -0,0 +1,34 @@
+package handler
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/HarsaEdu/harsa-api/internal/model/web"
+	"github.com/HarsaEdu/harsa-api/internal/pkg/res"
+	"github.com/HarsaEdu/harsa-api/internal/pkg/validation"
+	"github.com/labstack/echo/v4"
+)
+
+func (categoryHandler *CategoryHandlereImpl) Create(ctx echo.Context) error {
+
+	req := web.CategoryCreateRequest{}
+	err := ctx.Bind(&req)
+	if err != nil {
+		return res.StatusBadRequest(ctx, "failed to bind category request", err)
+	}
+
+	err = categoryHandler.CategoryService.Create(ctx, req)
+	if err != nil {
+		if strings.Contains(err.Error(), "validation") {
+			return validation.ValidationError(ctx, err)
+		}
+		if strings.Contains(err.Error(), "already exists") {
+			return res.StatusAlreadyExist(ctx, "category already exist", err)
+		}
+		return res.StatusInternalServerError(ctx, "failed to create category, something happen", fmt.Errorf("internal server error"))
+
+	}
+
+	return res.StatusCreated(ctx, "success to create category", nil)
+}
