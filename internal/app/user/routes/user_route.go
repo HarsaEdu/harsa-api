cc6322058b0d696c651c80a83a5f24177d2853bc
madya santosa <devmadya@gmail.com>
2023-11-14 22:59:34 +0800

    feat(user): add user and user profile route

--- internal/app/user/routes/user_route.go
+++ internal/app/user/routes/user_route.go
@@ -9,4 +9,6 @@ func (userRoutes *UserRoutesImpl) User(apiGroup *echo.Group) {
 	userGroup := apiGroup.Group("/users")
 
 	userGroup.POST("", userRoutes.UserHandler.UserCreate, middleware.AdminMiddleware)
+	userGroup.PUT("", userRoutes.UserHandler.UserUpdate, middleware.AdminMiddleware)
+	userGroup.PUT("/profile", userRoutes.UserHandler.UserProfileUpdate, middleware.AdminMiddleware)
 }
