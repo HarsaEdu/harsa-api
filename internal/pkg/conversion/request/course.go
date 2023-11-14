cadc17379ff9d46c6874d283754a1fa1d4f4d81f
AsZaychik <aszaychik@gmail.com>
2023-11-14 23:25:21 +0700

    feat(course): add course create request conversion

---
+++ internal/pkg/conversion/request/course.go
@@ -0,0 +1,16 @@
+package conversion
+
+import (
+	"github.com/HarsaEdu/harsa-api/internal/model/domain"
+	"github.com/HarsaEdu/harsa-api/internal/model/web"
+)
+
+func CourseCreateRequestToCourseDomain(request web.CourseCreateRequest, instructorId uint) *domain.Course {
+	return &domain.Course{
+		Title:       request.Title,
+		Description: request.Description,
+		ImageUrl: "https://placewaifu.com/image/200/200",
+		UserID: instructorId,
+		CategoryID: uint(request.CategoryID),
+	}
+}
