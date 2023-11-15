cf72329d8ecfe78dbc1fdda65e3518108b80051b
yusufUcps <yusuffy2212@gmail.com>
2023-11-15 14:04:41 +0700

    refactor(quiz): update quiz request

--- internal/model/web/question_request.go
+++ internal/model/web/question_request.go
@@ -3,10 +3,10 @@ package web
 import "github.com/HarsaEdu/harsa-api/internal/model/domain"
 
 type QuizRequest struct {
-	Module_id       uint                  `json:"module_id"`
-	User_id         uint                  `json:"user_id"`
-	Title           string                `gorm:"type:varchar(225)" json:"title"`
-	Description     string                `gorm:"type:text" json:"description"`
-	Durations       int                   `gorm:"type:int" json:"duration"`
-	Questions       []domain.Questions    `json:"questions"`
+	ModuleId    uint               `json:"module_id" validate:"required"`
+	UserId      uint               `json:"user_id" validate:"required"`
+	Title       string             `gorm:"type:varchar(225)" json:"title" validate:"required"`
+	Description string             `gorm:"type:text" json:"description" validate:"required"`
+	Durations   int                `gorm:"type:int" json:"duration"`
+	Questions   []domain.Questions `json:"questions"`
 }
