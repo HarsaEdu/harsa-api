f5a0651c8dc130092daad1c0c188aba46de9d6bc
AsZaychik <aszaychik@gmail.com>
2023-11-17 03:22:44 +0700

    feat(course): add course update request

--- internal/model/web/course_request.go
+++ internal/model/web/course_request.go
@@ -5,3 +5,9 @@ type CourseCreateRequest struct {
 	Description string `json:"description"`
 	CategoryID  int    `json:"category_id" validate:"required"`
 }
+
+type CourseUpdateRequest struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	CategoryID  int    `json:"category_id"`
+}
