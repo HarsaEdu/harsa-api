b33a441de9ffd396514af784eae66d0e96063ffa
AsZaychik <aszaychik@gmail.com>
2023-11-17 05:15:35 +0700

    feat(course): update course request accept form

--- internal/model/web/course_request.go
+++ internal/model/web/course_request.go
@@ -1,15 +1,15 @@
 package web
 
 type CourseCreateRequest struct {
-	Title       string `json:"title" validate:"required"`
-	Description string `json:"description"`
-	CategoryID  int    `json:"category_id" validate:"required"`
+	Title       string `json:"title" form:"title" validate:"required"`
+	Description string `json:"description" form:"description"`
+	CategoryID  int    `json:"category_id" form:"category_id" validate:"required"`
 }
 
 type CourseUpdateRequest struct {
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	CategoryID  int    `json:"category_id"`
+	Title       string `json:"title" form:"title"`
+	Description string `json:"description" form:"description"`
+	CategoryID  int    `json:"category_id" form:"category_id"`
 }
 
 type CourseUpdateImageRequest struct {
