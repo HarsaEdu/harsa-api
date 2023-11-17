05aa65adc52f49048017819b9f32fba690747135
arya <elprab17@gmail.com>
2023-11-17 15:55:30 +0700

    feat(profile): create profile validate image

--- internal/app/profile/service/profile_service_create_profile.go
+++ internal/app/profile/service/profile_service_create_profile.go
@@ -8,24 +8,24 @@ import (
 )
 
 func (profileService *ProfileServiceImpl) CreateProfile(ctx echo.Context, profile *domain.Profile) error {
-	fmt.Println(profile)
 	err := profileService.Validator.Struct(profile)
 	if err != nil {
 		return err
 	}
-	fmt.Println("image__url", profile.ImageURL)
 
-	_, errProfileExists := profileService.ProfileRepository.FindByUserID(profile.UserID)
-	if errProfileExists == nil {
+	profileExists, _ := profileService.ProfileRepository.FindByUserID(profile.UserID)
+	if profileExists != nil {
 		return fmt.Errorf("profile already exists")
 	}
 
-	profile.ImageURL, err = profileService.cloudinaryUploader.Uploader(ctx, "image", "profiles")
-	if err != nil {
-		return fmt.Errorf("error uploading image : %s", err.Error())
+	if image, _ := ctx.FormFile("image"); image == nil {
+		profile.ImageURL = ""
+	} else {
+		profile.ImageURL, err = profileService.cloudinaryUploader.Uploader(ctx, "image", "profiles")
+		if err != nil {
+			return fmt.Errorf("error uploading image : %s", err.Error())
+		}
 	}
-
-	fmt.Println("image__url", profile.ImageURL)
 
 	err = profileService.ProfileRepository.CreateProfile(profile)
 	if err != nil {
