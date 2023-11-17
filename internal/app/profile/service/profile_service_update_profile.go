6fc65b43299f1c2f8b600363621514b2b654da45
arya <elprab17@gmail.com>
2023-11-17 15:57:10 +0700

    refactor(profile): update profile validate image

--- internal/app/profile/service/profile_service_update_profile.go
+++ internal/app/profile/service/profile_service_update_profile.go
@@ -13,18 +13,18 @@ func (profileService *ProfileServiceImpl) UpdateProfile(ctx echo.Context, profil
 		return err
 	}
 
-	profileExists, _ := profileService.ProfileRepository.FindByUserID(profileID)
+	profileExists, _ := profileService.ProfileRepository.FindByUserID(profile.UserID)
 	if profileExists == nil {
 		return fmt.Errorf("profile not found")
 	}
-	//fix code below getting image from header
-	if file, _ := ctx.FormFile("image"); file != nil {
+
+	if image, _ := ctx.FormFile("image"); image == nil {
+		profile.ImageURL = profileExists.ImageURL
+	} else {
 		profile.ImageURL, err = profileService.cloudinaryUploader.Uploader(ctx, "image", "profiles")
 		if err != nil {
 			return fmt.Errorf("error uploading image : %s", err.Error())
 		}
-	} else {
-		profile.ImageURL = profileExists.ImageURL
 	}
 
 	err = profileService.ProfileRepository.UpdateProfile(profile, profileID)
