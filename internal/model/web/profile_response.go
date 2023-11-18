4ae34433eb5c6b50f1ab04865b3cab65877f0b43
arya <elprab17@gmail.com>
2023-11-18 23:53:19 +0700

    feat(profile): add get profile response

--- internal/model/web/profile_response.go
+++ internal/model/web/profile_response.go
@@ -1,15 +1,21 @@
 package web
 
+import (
+	"time"
+
+	"github.com/HarsaEdu/harsa-api/internal/model/domain"
+)
+
 type GetProfileResponse struct {
-	Class       string `gorm:"type:varchar(50)" json:"class"`
-	ImageURL    string `gorm:"type:varchar(255)" json:"image_url"`
-	FirstName   string `gorm:"type:varchar(50)" json:"first_name"`
-	LastName    string `gorm:"type:varchar(50)" json:"last_name"`
-	DateOfBirth int64  `gorm:"type:int" json:"date_of_birth"`
-	Bio         string `gorm:"type:varchar(255)" json:"bio"`
-	Gender      string `gorm:"type:enum('f','m')" json:"gender"`
-	PhoneNumber string `gorm:"type:varchar(20)" json:"phone_number"`
-	City        string `gorm:"type:varchar(20)" json:"city" form:"city"`
-	Address     string `gorm:"type:varchar(255)" json:"address"`
-	Job         string `gorm:"type:varchar(20)" json:"job"`
+	Class       string        `gorm:"type:varchar(50)" json:"class"`
+	ImageUrl    string        `gorm:"type:varchar(255)" json:"image_url"`
+	FirstName   string        `gorm:"type:varchar(50)" json:"first_name"`
+	LastName    string        `gorm:"type:varchar(50)" json:"last_name"`
+	DateBirth   time.Time     `gorm:"type:int" json:"date_birth"`
+	Bio         string        `gorm:"type:varchar(255)" json:"bio"`
+	Gender      domain.Gender `gorm:"type:enum('f','m')" json:"gender"`
+	PhoneNumber string        `gorm:"type:varchar(20)" json:"phone_number"`
+	City        string        `gorm:"type:varchar(20)" json:"city" form:"city"`
+	Address     string        `gorm:"type:varchar(255)" json:"address"`
+	Job         string        `gorm:"type:varchar(20)" json:"job"`
 }
