7e398c8aee9854d9478a64e30097dedbf12d848a
DimasAnugerahh <dimasanugrah883@gmail.com>
2023-11-18 14:02:17 +0800

    reafactor(categories): pagination functionality

--- internal/app/categories/repository/category_repository_get_all.go
+++ internal/app/categories/repository/category_repository_get_all.go
@@ -2,13 +2,24 @@ package repository
 
 import "github.com/HarsaEdu/harsa-api/internal/model/domain"
 
-func (categoryRepository *CategoryRepositoryImpl) GetAll() ([]domain.Category, error) {
+func (categoryRepository *CategoryRepositoryImpl) GetAll(offset, limit int, search string) ([]domain.Category, int64, error) {
 	category := []domain.Category{}
+	var total int64
 
-	result := categoryRepository.DB.Find(&category)
-	if result.Error != nil {
-		return nil, result.Error
+	query := categoryRepository.DB.Model(&category)
+
+	if search != "" {
+		s := "%" + search + "%"
+		query = query.Where("categories.name LIKE ? OR categories.description LIKE ?", s, s)
 	}
 
-	return category, nil
+	query.Find(&category).Count(&total)
+
+	query = query.Offset(offset).Limit(limit)
+
+	if query.Error != nil {
+		return nil, 0, query.Error
+	}
+
+	return category, total, nil
 }
