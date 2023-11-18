4dc559891c9d28e101783ac81e9f75f56aa8389c
yusufUcps <yusuffy2212@gmail.com>
2023-11-18 22:34:29 +0700

    feat(quizzes): Added cek user id

---
+++ internal/app/quizzes/repository/quizzes_repository_cek_by_user_id.go
@@ -0,0 +1,35 @@
+package repository
+
+import (
+	"fmt"
+
+	"github.com/HarsaEdu/harsa-api/internal/model/domain"
+)
+
+func (repository *QuizzesRepositoryImpl) CekId(userId uint, quizId uint, role string) (*domain.Quizzes, error) {
+
+	var quiz = domain.Quizzes{}
+
+	if err := repository.DB.First(&quiz, quizId).Error; err != nil {
+		return nil, err
+	}
+
+	var module = domain.Modules{}
+
+	if err := repository.DB.First(&module, quiz.ModuleId).Error; err != nil {
+		return nil, err
+	}
+
+	var course = domain.Course{}
+
+	if err := repository.DB.First(&course, module.CourseId).Error; err != nil {
+		return nil, err
+	}
+
+	if course.UserID != userId && role != "admin" {
+		return nil, fmt.Errorf("unauthorized")
+	}
+	
+
+	return &quiz, nil
+}
