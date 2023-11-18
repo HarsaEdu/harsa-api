ab95a8c59f882341ff9bbc868caf1d12d10f04d4
yusufUcps <yusuffy2212@gmail.com>
2023-11-18 22:35:01 +0700

    feat(quizzes): Added update feature

---
+++ internal/app/quizzes/repository/quizzes_repository_update.go
@@ -0,0 +1,36 @@
+package repository
+
+import "github.com/HarsaEdu/harsa-api/internal/model/domain"
+
+func (repository *QuizzesRepositoryImpl) Update(UpdateQuiz *domain.Quizzes, quizExist *domain.Quizzes) error {
+
+	tx := repository.DB.Begin()
+
+	quizExist.ModuleId = UpdateQuiz.ModuleId
+	quizExist.Title = UpdateQuiz.Title
+	quizExist.Description = UpdateQuiz.Description
+	quizExist.Durations = UpdateQuiz.Durations
+
+	if err := tx.Save(&quizExist).Error; err != nil {
+		repository.DB.Rollback()
+		return err
+	}
+
+	for _, updatedQuestion := range UpdateQuiz.Questions {
+        if err := tx.Save(&updatedQuestion).Error; err != nil {
+            tx.Rollback()
+            return err
+        }
+
+        for _, updatedOption := range updatedQuestion.Options {
+            if err := tx.Save(&updatedOption).Error; err != nil {
+                tx.Rollback()
+                return err
+            }
+        }
+    }
+
+    tx.Commit()
+
+	return nil
+}
---
+++ internal/app/quizzes/service/quizzes_service_update.go
@@ -0,0 +1,31 @@
+package service
+
+import (
+	"fmt"
+
+	"github.com/HarsaEdu/harsa-api/internal/model/web"
+	conversionRequest "github.com/HarsaEdu/harsa-api/internal/pkg/conversion/request"
+	"github.com/labstack/echo/v4"
+)
+
+func (quizzesService *QuizzesServiceImpl) Update(ctx echo.Context, request web.QuizRequest, quizId uint, role string) error {
+	err := quizzesService.Validator.Struct(request)
+	if err != nil {
+		return err
+	}
+
+	quiz := conversionRequest.QuizCreateRequestToQuizzesModel(request)
+
+	quizExist, err := quizzesService.QuizzesRepository.CekId(request.UserId, quizId, role)
+	if err != nil { 
+		return fmt.Errorf("error when cek id user in quiz update :%s", err.Error())
+	}
+
+	err = quizzesService.QuizzesRepository.Update(quiz, quizExist)
+	if err != nil {
+		return fmt.Errorf("error when update quiz %s", err.Error())
+	} 
+
+	return nil
+
+}
