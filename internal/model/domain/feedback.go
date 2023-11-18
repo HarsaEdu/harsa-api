c308fd469b4a34ee4c8edcdc40beb7f4599fe20a
Alhadji01 <lilingponsel2016@gmail.com>
2023-11-18 22:25:44 +0700

    feat(feedback): add feedback domain struct

---
+++ internal/model/domain/feedback.go
@@ -0,0 +1,18 @@
+package domain
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Feedback struct {
+	ID        uint           `gorm:"type:int;primarykey" json:"id"`
+	UserID    uint           `gorm:"type:int" json:"user_id"`
+	CourseID  uint           `gorm:"type:int" json:"course_id"`
+	Rating    int            `gorm:"type:int" json:"rating"`
+	Content   string         `gorm:"type:text" json:"content"`
+	CreatedAt time.Time      `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"delete_at"`
+}
