030666fdd97cfabdee4bad91a27185751700d0ea
DimasAnugerahh <dimasanugrah883@gmail.com>
2023-11-19 14:58:05 +0800

    refactor(categories): update pagination

--- internal/app/categories/repository/category_repository_get_all.go
+++ internal/app/categories/repository/category_repository_get_all.go
@@ -3,28 +3,37 @@ package repository
 import "github.com/HarsaEdu/harsa-api/internal/model/domain"
 
 func (categoryRepository *CategoryRepositoryImpl) GetAll(offset, limit int, search string) ([]domain.Category, int64, error) {
+
+	if offset < 0 || limit < 0 {
+		return nil, 0, nil
+	}
+
 	category := []domain.Category{}
 	var total int64
-
-	if limit < 0 || offset < 0 {
-		return nil, 0, nil
-
-	}
 
 	query := categoryRepository.DB.Model(&category)
 
 	if search != "" {
 		s := "%" + search + "%"
-		query = query.Where("categories.name LIKE ? OR categories.description LIKE ?", s, s)
+		query = query.Where("name LIKE ? OR description LIKE ?", s, s)
 	}
 
 	query.Find(&category).Count(&total)
+
 	query = query.Limit(limit).Offset(offset)
 
 	result := query.Find(&category)
 
 	if result.Error != nil {
 		return nil, 0, result.Error
+	}
+
+	if total == 0 {
+		return nil, 0, nil
+	}
+
+	if offset >= int(total) {
+		return nil, 0, nil
 	}
 
 	return category, total, nil
