85990a2be579c006bcdafd7ff77d790196f48b53
DimasAnugerahh <dimasanugrah883@gmail.com>
2023-11-20 02:46:05 +0800

    feat(faqs): add repository interface

--- internal/app/faqs/repository/faqs_repository_interface.go
+++ internal/app/faqs/repository/faqs_repository_interface.go
@@ -1,1 +1,22 @@
 package repository
+
+import (
+	"github.com/HarsaEdu/harsa-api/internal/model/domain"
+	"gorm.io/gorm"
+)
+
+type FaqRepository interface {
+	Create(Faq *domain.Faqs) error
+	GetAll(offset, limit int, search string) ([]domain.Faqs, int64, error)
+	Update(Faq *domain.Faqs, id int) error
+	Delete(id int) error
+	FindById(id int) (*domain.Faqs, error)
+}
+
+type FaqRepositoryImpl struct {
+	DB *gorm.DB
+}
+
+func NewFaqRepository(db *gorm.DB) FaqRepository {
+	return &FaqRepositoryImpl{DB: db}
+}
