db1bcda0021d2d48befe13b841cffae9367f8086
Alhadji01 <lilingponsel2016@gmail.com>
2023-11-20 15:03:49 +0700

    feat(feedback) : added feedback service interface

---
+++ internal/app/feedback/service/feedback_service_interface.go
@@ -0,0 +1,25 @@
+package service
+
+import (
+	"github.com/HarsaEdu/harsa-api/internal/app/feedback/repository"
+	"github.com/HarsaEdu/harsa-api/internal/model/domain"
+	"github.com/HarsaEdu/harsa-api/internal/model/web"
+	"github.com/go-playground/validator"
+)
+
+type FeedbackService interface {
+	Create(feedback web.FeedbackCreateRequest) error
+	Update(feedback web.FeedbackUpdateRequest, id int) error
+	FindById(id int) (*domain.Feedback, error)
+	GetAll(offset, limit int, search string) ([]domain.Feedback, *web.Pagination, error)
+	Delete(id int) error
+}
+
+type FeedbackServiceImpl struct {
+	FeedbackRepository repository.FeedbackRepository
+	Validator          *validator.Validate
+}
+
+func NewFeedbackService(cr repository.FeedbackRepository, validate *validator.Validate) FeedbackService {
+	return &FeedbackServiceImpl{FeedbackRepository: cr, Validator: validate}
+}
