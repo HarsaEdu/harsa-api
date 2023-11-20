b81f8f743031d49ff18857913966cf083492bca6
Alhadji01 <lilingponsel2016@gmail.com>
2023-11-20 14:24:23 +0700

    refactor(feedback): update feedback model

--- internal/model/domain/feedback.go
+++ internal/model/domain/feedback.go
@@ -12,6 +12,8 @@ type Feedback struct {
 	CourseID  uint           `gorm:"type:int" json:"course_id"`
 	Rating    int            `gorm:"type:int" json:"rating"`
 	Content   string         `gorm:"type:text" json:"content"`
+	User      User           `gorm:"foreignKey:UserID;references:ID"`
+	Course    Course         `gorm:"foreignKey:CourseID;references:ID"`
 	CreatedAt time.Time      `gorm:"autoCreateTime" json:"created_at"`
 	UpdatedAt time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"delete_at"`
