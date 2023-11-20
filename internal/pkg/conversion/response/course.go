bf00774870a2f7c5164dec3c61cd1003ad8c9bbc
yusufUcps <yusuffy2212@gmail.com>
2023-11-20 12:28:34 +0700

    refactor(course): update response

--- internal/pkg/conversion/response/course.go
+++ internal/pkg/conversion/response/course.go
@@ -10,28 +10,30 @@ func CourseDomainToCourseGetAllResponse(courseDomain []domain.Course) []web.GetC
 
 	for _, course := range courseDomain {
 
-		courseGetAllResponse = append(courseGetAllResponse, web.GetCourseResponse{
-			ID:          course.ID,
-			Title:       course.Title,
-			Description: course.Description,
-			ImageUrl:    course.ImageUrl,
-			Enrolled:    course.Enrolled,
-			Rating:      course.Rating,
-			CreatedAt:   course.CreatedAt,
-			UpdatedAt:   course.UpdatedAt,
-			Category: &web.CategoryForCourseResponse{
-				ID:   course.Category.ID,
-				Name: course.Category.Name,
-			},
-			User: &web.UserForCourseResponse{
-				ID:    course.User.ID,
-				Email: course.User.Email,
-				Role: web.RoleForUserForCourseResponse{
-					ID:   course.User.Role.ID,
-					Name: course.User.Role.Name,
+		if course.Category.ID != 0 {
+			courseGetAllResponse = append(courseGetAllResponse, web.GetCourseResponse{
+				ID:          course.ID,
+				Title:       course.Title,
+				Description: course.Description,
+				ImageUrl:    course.ImageUrl,
+				Enrolled:    course.Enrolled,
+				Rating:      course.Rating,
+				CreatedAt:   course.CreatedAt,
+				UpdatedAt:   course.UpdatedAt,
+				Category: &web.CategoryForCourseResponse{
+					ID:   course.Category.ID,
+					Name: course.Category.Name,
 				},
-			},
-		})
+				User: &web.UserForCourseResponse{
+					ID:    course.User.ID,
+					Email: course.User.Email,
+					Role: web.RoleForUserForCourseResponse{
+						ID:   course.User.Role.ID,
+						Name: course.User.Role.Name,
+					},
+				},
+			})
+		}
 	}
 
 	return courseGetAllResponse
