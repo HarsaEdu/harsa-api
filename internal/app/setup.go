88668d4bd20dc1d7b2d486474043a7f3d5d07b1b
DimasAnugerahh <dimasanugrah883@gmail.com>
2023-11-22 22:59:36 +0800

    refactor: update set up

---
+++ internal/app/setup.go
@@ -0,0 +1,47 @@
+package app
+
+import (
+	auth "github.com/HarsaEdu/harsa-api/internal/app/auth"
+	category "github.com/HarsaEdu/harsa-api/internal/app/categories"
+	course "github.com/HarsaEdu/harsa-api/internal/app/course"
+	faqs "github.com/HarsaEdu/harsa-api/internal/app/faqs"
+	module "github.com/HarsaEdu/harsa-api/internal/app/module"
+	subsPlan "github.com/HarsaEdu/harsa-api/internal/app/subs_plan"
+	user "github.com/HarsaEdu/harsa-api/internal/app/user"
+
+	"github.com/HarsaEdu/harsa-api/internal/pkg/cloudinary"
+	"github.com/go-playground/validator"
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
+func InitApp(db *gorm.DB, validate *validator.Validate, cloudinary cloudinary.CloudinaryUploader, e *echo.Echo) {
+
+	userRoutes, userRepo := user.UserSetup(db, validate)
+	authRoutes := auth.AuthSetup(db, validate, userRepo)
+	moduleRoutes := module.ModuleSetup(db, validate)
+	categoryRoutes := category.CategorySetup(db, validate, cloudinary)
+	faqsRoutes := faqs.FaqsSetup(db, validate)
+	courseRoutes := course.CourseSetup(db, validate, cloudinary)
+	subsPlanRoutes := subsPlan.SubsPlanSetup(db, validate, cloudinary)
+
+	apiGroupWeb := e.Group("web")
+	authRoutes.AuthWeb(apiGroupWeb)
+	userRoutes.UserWeb(apiGroupWeb)
+	categoryRoutes.CategoryWeb(apiGroupWeb)
+	courseRoutes.CourseWeb(apiGroupWeb)
+	faqsRoutes.FaqsWeb(apiGroupWeb)
+	subsPlanRoutes.SubsPlanWeb(apiGroupWeb)
+	coursesGroup := courseRoutes.CourseWeb(apiGroupWeb)
+	moduleRoutes.ModuleWeb(coursesGroup)
+
+	apiGroupMobile := e.Group("mobile")
+	authRoutes.AuthMobile(apiGroupMobile)
+	userRoutes.UserMobile(apiGroupMobile)
+	categoryRoutes.CategoryMobile(apiGroupMobile)
+	courseRoutes.CourseMobile(apiGroupMobile)
+	faqsRoutes.FaqsMobile(apiGroupMobile)
+	subsPlanRoutes.SubsPlanMobile(apiGroupMobile)
+	coursesGroup = courseRoutes.CourseMobile(apiGroupMobile)
+	moduleRoutes.ModuleMobile(apiGroupMobile)
+}
