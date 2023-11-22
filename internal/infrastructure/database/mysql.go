383f767a40a67001da8311c7baf005014d3de4a8
AsZaychik <aszaychik@gmail.com>
2023-11-23 00:17:49 +0700

    feat(mysql) : add migrate domain user chat topic

--- internal/infrastructure/database/mysql.go
+++ internal/infrastructure/database/mysql.go
@@ -28,5 +28,5 @@ func NewMySQLConnection(config *configs.MySQLConfig) (*gorm.DB, error) {
 
 func migration(db *gorm.DB) {
 
-	db.AutoMigrate(&domain.Category{}, &domain.User{}, &domain.Role{}, &domain.UserProfile{}, &domain.Course{}, &domain.Module{}, &domain.SubModule{}, &domain.Faqs{})
+	db.AutoMigrate(&domain.Category{}, &domain.User{}, &domain.Role{}, &domain.UserProfile{}, &domain.Course{}, &domain.Module{}, &domain.SubModule{}, &domain.Faqs{}, &domain.UserChatTopic{})
 }
