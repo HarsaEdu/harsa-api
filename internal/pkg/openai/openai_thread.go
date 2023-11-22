6933a2d07b9c0aacbc122947cb00384e5789b1f0
AsZaychik <aszaychik@gmail.com>
2023-11-23 00:22:07 +0700

    refactor(openai): update thread create request

--- internal/pkg/openai/openai_thread.go
+++ internal/pkg/openai/openai_thread.go
@@ -12,19 +12,16 @@ func (openAi *OpenAiImpl) CreateThread(studentName string, topic string) (string
 
 	content := fmt.Sprintf("Hi! I, %s, am seeking assistance on the topic of %s. Can you please provide guidance and support?", studentName, topic)
 
-	threadTopic := openai.MessageRequest{
+	threadTopic := openai.ThreadMessage{
 		Role: openai.ChatMessageRoleUser,
 		Content: content,
 	}
 
-	thread, err := openAi.Client.CreateThread(ctx, openai.ThreadRequest{})
-	if err != nil {
-		return "", err
-	}
-
-	fmt.Println(thread)
-
-	_, err = openAi.Client.CreateMessage(ctx, thread.ID, threadTopic)
+	thread, err := openAi.Client.CreateThread(ctx, openai.ThreadRequest{
+		Messages: []openai.ThreadMessage{
+			threadTopic,
+		},
+	})
 	if err != nil {
 		return "", err
 	}
