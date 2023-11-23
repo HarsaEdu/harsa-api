cdb7d9e240707c78ae7503feb4198589aee28340
yusufUcps <yusuffy2212@gmail.com>
2023-11-23 15:01:40 +0700

    refactor(quizzes): update model

--- internal/model/domain/quiz.go
+++ internal/model/domain/quiz.go
@@ -12,7 +12,7 @@ type Quizzes struct {
 	Title       string         `gorm:"type:varchar(225)" json:"title"`
 	Description string         `gorm:"type:text" json:"description"`
 	Durations   int            `gorm:"type:int" json:"duration"`
-	Questions   []Questions    `json:"questions" gorm:"foreignKey:QuizId"`
+	Questions   []Questions    `json:"questions" gorm:"foreignKey:QuizID"`
 	CreatedAt   time.Time      `gorm:"autoCreateTime" json:"created_at"`
 	UpdatedAt   time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
 	DeletedAt   gorm.DeletedAt `json:"deleted_at"`
