71c72469a4d377b50ce17ee83e546077e1200aa7
AsZaychik <aszaychik@gmail.com>
2023-11-23 11:40:47 +0700

    feat(chatbot): add get all thread by user response

--- internal/model/web/chatbot_response.go
+++ internal/model/web/chatbot_response.go
@@ -1,5 +1,15 @@
 package web
 
+import "time"
+
+type GetAllThreadByUserIdResponse struct {
+	ID        string   `json:"id" form:"id"`
+	UserId    int      `json:"user_id" form:"user_id"`
+	Topic     string   `json:"topic" form:"topic"`
+	CreatedAt time.Time `json:"created_at" form:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" form:"updated_at"`
+}
+
 type GetMessageInThreadResponse struct {
 	ID        string `json:"id" form:"id"`
 	Role      string `json:"role" form:"role"`
