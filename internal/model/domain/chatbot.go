bfdd3d6ee653914c1f3051f0fcf78fa3a8d46945
AsZaychik <aszaychik@gmail.com>
2023-11-23 11:28:39 +0700

    feat(chatbot) : update domain relation user

--- internal/model/domain/chatbot.go
+++ internal/model/domain/chatbot.go
@@ -8,8 +8,10 @@ import (
 
 type UserChatTopic struct {
 	ID        string         `gorm:"type:varchar(255);primarykey" json:"id"`
+	UserID    uint           `gorm:"type:varchar(255)" json:"user_id"`
 	Topic     string         `gorm:"type:varchar(255)" json:"topic"`
 	CreatedAt time.Time      `gorm:"autoCreateTime" json:"created_at"`
 	UpdatedAt time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"delete_at"`
+	User      User           `gorm:"foreignKey:UserID"`
 }
