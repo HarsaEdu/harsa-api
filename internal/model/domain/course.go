65ba5315b269136f485e6559e7c1dc35798db2bd
yusufUcps <yusuffy2212@gmail.com>
2023-11-24 11:18:26 +0700

    refactor(course): udapte domain

--- internal/model/domain/course.go
+++ internal/model/domain/course.go
@@ -7,18 +7,18 @@ import (
 )
 
 type Course struct {
-	ID          uint           `gorm:"type:int;primarykey" json:"id"`
-	UserID      uint           `gorm:"type:int" json:"user_id"`
-	Title       string         `gorm:"type:varchar(255)" json:"title"`
-	Description string         `gorm:"type:text" json:"description"`
-	Enrolled    int            `gorm:"type:int" json:"enrolled"`
-	Rating      int            `gorm:"type:int" json:"rating"`
-	ImageUrl    string         `gorm:"type:varchar(255)" json:"image_url"`
-	CreatedAt   time.Time      `gorm:"autoCreateTime" json:"created_at"`
-	UpdatedAt   time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
-	DeletedAt   gorm.DeletedAt `gorm:"index" json:"delete_at"`
-	CategoryID  uint           `gorm:"type:int" json:"category_id"`
-	User        UserProfile    `gorm:"foreignKey:UserID;references:UserID"`
-	Category    Category       `gorm:"foreignKey:CategoryID;references:ID"`
-	Modules     []*Module      `gorm:"foreignKey:CourseID;references:ID"`
+	ID                 uint           `gorm:"type:int;primarykey" json:"id"`
+	UserID             uint           `gorm:"type:int" json:"user_id"`
+	Title              string         `gorm:"type:varchar(255)" json:"title"`
+	Description        string         `gorm:"type:text" json:"description"`
+	Enrolled           int            `gorm:"type:int" json:"enrolled"`
+	Rating             int            `gorm:"type:int" json:"rating"`
+	ImageUrl           string         `gorm:"type:varchar(255)" json:"image_url"`
+	CreatedAt          time.Time      `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt          time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
+	DeletedAt          gorm.DeletedAt `gorm:"index" json:"delete_at"`
+	CategoryID         uint           `gorm:"type:int" json:"category_id"`
+	User               UserProfile    
+	Category           Category       `gorm:"foreignKey:CategoryID;references:ID"`
+	Modules            []*Module      `gorm:"foreignKey:CourseID;references:ID"`
 }
