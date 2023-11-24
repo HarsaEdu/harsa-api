7987dbc3163d3ea8a6317b098dc1a6c6dc7a0a27
AsZaychik <aszaychik@gmail.com>
2023-11-24 10:20:12 +0700

    feat(chatbot): add chat with assistant request

--- internal/model/web/chatbot_request.go
+++ internal/model/web/chatbot_request.go
@@ -7,3 +7,7 @@ type CreateThreadRequest struct {
 type GetResponseRequest struct {
 	Message string `json:"message" form:"message" validate:"required"`
 }
+
+type ChatWithAssistantRequest struct {
+	Message string `json:"message" form:"message" validate:"required"`
+}
