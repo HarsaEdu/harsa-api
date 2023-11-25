138441240aa51abd285cbbc010aec56f0adc9f4c
yusufUcps <yusuffy2212@gmail.com>
2023-11-25 09:59:27 +0700

    refactor(course): refactor repository get by id

--- internal/app/course/repository/course_repository_get_by_id.go
+++ internal/app/course/repository/course_repository_get_by_id.go
@@ -2,10 +2,14 @@ package repository
 
 import "github.com/HarsaEdu/harsa-api/internal/model/domain"
 
-func (courseRepository *CourseRepositoryImpl) GetById(id uint) (*domain.Course, error) {
-	course := &domain.Course{}
+func (courseRepository *CourseRepositoryImpl) GetById(id uint) (*domain.CourseEntity, error) {
+	course := &domain.CourseEntity{}
 
-	result := courseRepository.DB.Preload("User.Role").Preload("Category").Preload("Modules.SubModules").First(&course, id)
+	result := courseRepository.DB.Model(&domain.Course{}).Select("courses.id as id, title, courses.description as description, enrolled, rating, courses.image_url as image_url,  courses.created_at as created_at, courses.updated_at as updated_at, roles.name as role_name, user_profiles.user_id as user_id, user_profiles.first_name as first_name, user_profiles.last_name as last_name, user_profiles.job as job, categories.id as category_id, categories.name as category_name").
+	Joins("left join user_profiles on user_profiles.user_id = courses.user_id").
+	Joins("left join users on users.id = user_profiles.user_id").
+	Joins("left join roles on roles.id = users.role_id"). 
+	Joins("left join categories on categories.id = courses.category_id").Where("courses.id = ?", id).Find(&course)
 	if result.Error != nil {
 		return nil, result.Error
 	}
