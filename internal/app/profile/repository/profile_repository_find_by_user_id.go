a84ca6668ec56d00c5e9cb81cff5a0b8dee8fcae
arya <elprab17@gmail.com>
2023-11-26 12:22:54 +0700

    refactor(profile): change result to user detail

--- internal/app/profile/repository/profile_repository_find_by_user_id.go
+++ internal/app/profile/repository/profile_repository_find_by_user_id.go
@@ -2,13 +2,18 @@ package repository
 
 import "github.com/HarsaEdu/harsa-api/internal/model/domain"
 
-func (profileRepository *ProfileRepositoryImpl) FindByUserID(userID uint) (*domain.UserProfile, error) {
-	profile := domain.UserProfile{}
+func (profileRepository *ProfileRepositoryImpl) FindByUserID(userID uint) (*domain.UserDetail, error) {
+	user := &domain.UserDetail{}
 
-	result := profileRepository.DB.Where("user_id = ?", userID).First(&profile)
+	result := profileRepository.DB.Model(&domain.User{}).Select("users.id as user_id, user_profiles.id as user_profile_id, roles.id as role_id, email, username, phone_number, roles.name as role_name, first_name, last_name, bio, address, city, gender, job, date_birth").
+		Joins("left join user_profiles on user_profiles.user_id = users.id").
+		Joins("left join roles on roles.id = users.role_id").
+		Where("users.id = ?", userID).
+		First(&user)
+
 	if result.Error != nil {
 		return nil, result.Error
 	}
 
-	return &profile, nil
+	return user, nil
 }
