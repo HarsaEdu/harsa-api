464df81952ef9bb783dec7428774cec82553625d
arya <elprab17@gmail.com>
2023-11-27 21:15:24 +0700

    feat(history sub module): add domain

---
+++ internal/model/domain/history_sub_module.go
@@ -0,0 +1,8 @@
+package domain
+
+type HistorySubModule struct {
+	ID          uint `gorm:"primaryKey" json:"id"`
+	SubModuleID uint `gorm:"type:int" json:"module_id"`
+	UserID      uint `gorm:"type:int" json:"user_id"`
+	IsCompleted bool `json:"is_completed"`
+}
