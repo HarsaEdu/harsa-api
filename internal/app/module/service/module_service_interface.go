694a6d7e2204846caeb70aeb713c7fdaca71e986
yusufUcps <yusuffy2212@gmail.com>
2023-11-27 17:56:23 +0700

    update(module): update get

--- internal/app/module/service/module_service_interface.go
+++ internal/app/module/service/module_service_interface.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"github.com/HarsaEdu/harsa-api/internal/app/module/repository"
-	"github.com/HarsaEdu/harsa-api/internal/model/domain"
 	"github.com/HarsaEdu/harsa-api/internal/model/web"
 	"github.com/go-playground/validator"
 	"github.com/labstack/echo/v4"
@@ -10,7 +9,7 @@ import (
 
 type ModuleService interface {
 	Create(ctx echo.Context, request *web.ModuleRequest, courseId uint, userId uint, role string) error
-	GetAllByCourseId(offset, limit int, search string, courseId uint) ([]domain.Module, *web.Pagination, error)
+	GetAllByCourseId(offset, limit int, search string, courseId uint) ([]web.ModuleResponse, *web.Pagination, error)
 	// GetById(id uint) (*web.GetModuleResponse, error)
 	// Update(id uint, request *web.ModuleUpdateRequest) error
 	// UpdateImage(ctx echo.Context, id uint, request *web.ModuleUpdateImageRequest) error
