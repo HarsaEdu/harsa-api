fc8d4a96ad736ab6e5f132fc6117ef3ea7dea08f
AsZaychik <aszaychik@gmail.com>
2023-11-28 09:38:21 +0700

    feat(midtrans): update charge request

--- internal/pkg/midtrans/midtrans_core_api_interface.go
+++ internal/pkg/midtrans/midtrans_core_api_interface.go
@@ -2,14 +2,12 @@ package midtrans
 
 import (
 	"github.com/HarsaEdu/harsa-api/configs"
-	"github.com/HarsaEdu/harsa-api/internal/model/domain"
 	"github.com/midtrans/midtrans-go"
 	"github.com/midtrans/midtrans-go/coreapi"
 )
 
 type MidtransCoreApi interface {
-	CreateTransaction(subscription *domain.SubsPlan, customer *domain.UserDetail) (*coreapi.ChargeResponse, error)
-	chargeTransaction(request *coreapi.ChargeReq) (*coreapi.ChargeResponse, error)
+	ChargeTransaction(request *coreapi.ChargeReq) (*coreapi.ChargeResponse, error)
 }
 
 type MidtransCoreApiImpl struct {
