f081e33007468b53c75575c634fd08e1c6402e4e
Alhadji01 <lilingponsel2016@gmail.com>
2023-11-28 12:19:54 +0700

    chore(history_module): fix setup

--- internal/app/history_module/historymodule_setup.go
+++ internal/app/history_module/historymodule_setup.go
@@ -1,1 +1,19 @@
-package historymodule
+package faqs
+
+import (
+	historyModuleHandlerPkg "github.com/HarsaEdu/harsa-api/internal/app/history_module/handler"
+	historyModuleRepositoryPkg "github.com/HarsaEdu/harsa-api/internal/app/history_module/repository"
+	historyModuleRoutesPkg "github.com/HarsaEdu/harsa-api/internal/app/history_module/routes"
+	historyModuleServicePkg "github.com/HarsaEdu/harsa-api/internal/app/history_module/service"
+	"github.com/go-playground/validator"
+	"gorm.io/gorm"
+)
+
+func HistoryModuleSetup(db *gorm.DB, validate *validator.Validate) historyModuleRoutesPkg.HistoryModuleRoutes {
+	historyModuleRepository := historyModuleRepositoryPkg.NewHistoryModuleRepository(db)
+	historyModuleService := historyModuleServicePkg.NewHistoryModuleService(historyModuleRepository, validate)
+	historyModuleHandler := historyModuleHandlerPkg.NewHistoryModuleHandler(historyModuleService)
+	historyModuleRoute := historyModuleRoutesPkg.NewHistoryModuleRoutes(historyModuleHandler)
+
+	return historyModuleRoute
+}
