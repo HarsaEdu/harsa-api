1f69d55f18869035b6f51c1a387d6f4d3a1a5764
yusufUcps <yusuffy2212@gmail.com>
2023-11-28 17:43:23 +0700

    feat(course_Tracking): add enrolled feature

---
+++ internal/app/course_tracking/handler/course_tracking_handler_create.go
@@ -0,0 +1,39 @@
+package handler
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/HarsaEdu/harsa-api/internal/model/web"
+	"github.com/HarsaEdu/harsa-api/internal/pkg/res"
+	"github.com/HarsaEdu/harsa-api/internal/pkg/validation"
+	"github.com/labstack/echo/v4"
+)
+
+func (courseTrackingHandler *CourseTrackingHandlerImpl) Create(ctx echo.Context) error {
+
+	req := web.CourseTrackingRequest{}
+
+	id := ctx.Get("user_id").(uint)
+
+	idParam := ctx.Param("course-id")
+	courseId, err := strconv.Atoi(idParam)
+	if err != nil {
+		return res.StatusBadRequest(ctx, "invalid course id", err)
+	}
+	
+
+	req.CourseID = uint(courseId)
+	req.UserID = id
+	req.Status = "in_progress"
+	err = courseTrackingHandler.CourseTrackingService.Create(req)
+	if err != nil {
+		if strings.Contains(err.Error(), "validation") {
+			return validation.ValidationError(ctx, err)
+		}
+		return res.StatusInternalServerError(ctx, "failed to create course tracking, something happen", err)
+
+	}
+
+	return res.StatusCreated(ctx, "success to create course tracking", nil, nil)
+}
