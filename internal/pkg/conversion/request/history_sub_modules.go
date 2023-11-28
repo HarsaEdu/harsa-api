82cd28426ebd7a7e2cd92bef94b6497c8f4b26a6
arya <elprab17@gmail.com>
2023-11-28 09:12:42 +0700

    feat(history sub module): add get conversion

---
+++ internal/pkg/conversion/request/history_sub_modules.go
@@ -0,0 +1,14 @@
+package conversion
+
+import (
+	"github.com/HarsaEdu/harsa-api/internal/model/domain"
+	"github.com/HarsaEdu/harsa-api/internal/model/web"
+)
+
+func CreateHistorySubModuleRequestToModel(request *web.CreateHistorySubModuleRequest, userID uint) *domain.HistorySubModule {
+	return &domain.HistorySubModule{
+		SubModuleID: request.SubModuleID,
+		UserID:      userID,
+		IsCompleted: request.IsCompleted,
+	}
+}
