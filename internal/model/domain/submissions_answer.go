a325239ce1cbff58f7cd59db7be2469ca799295a
DimasAnugerahh <dimasanugrah883@gmail.com>
2023-12-01 02:37:11 +0800

    func(subAns): add submission ans model

---
+++ internal/model/domain/submissions_answer.go
@@ -0,0 +1,19 @@
+package domain
+
+type Status string
+
+const (
+	Accepted  Status = "Accepted"
+	Pending   Status = "Pending"
+	Review    Status = "Review"
+	Rejected  Status = "Rejected"
+	NotSubmit Status = "NotSubmit"
+)
+
+type SubmissionsAnswer struct {
+	ID             uint   `gorm:"type:int;primarykey" json:"id"`
+	SubsmissionsID uint   `json:"subsmission_id" `
+	Content        string `gorm:"type:text" json:"content"`
+	Status         Status `json:"status" gorm:"type:varchar(255)"`
+	Feedback       string `json:"feedback"`
+}
