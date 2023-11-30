7cd21e29388f1de5545d12f7dab9634680742e64
DimasAnugerahh <dimasanugrah883@gmail.com>
2023-12-01 02:35:23 +0800

    feat(submission:) add handler interface

---
+++ internal/app/submission/handler/submission_handler_interface.go
@@ -0,0 +1,20 @@
+package handler
+
+import (
+	"github.com/HarsaEdu/harsa-api/internal/app/submission/service"
+	"github.com/labstack/echo/v4"
+)
+
+type SubmissionHandler interface {
+	Create(ctx echo.Context) error
+	Update(ctx echo.Context) error
+	Delete(ctx echo.Context) error
+}
+
+type SubmissionHandlerImpl struct {
+	SubmissionService service.SubmissionService
+}
+
+func NewSubmissionHandler(service service.SubmissionService) SubmissionHandler {
+	return &SubmissionHandlerImpl{SubmissionService: service}
+}
