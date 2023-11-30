439d4df0b2bd426f4e7c01b5d3d09e2eca578884
DimasAnugerahh <dimasanugrah883@gmail.com>
2023-12-01 02:35:44 +0800

    feat(submission:) add service interface

---
+++ internal/app/submission/service/submission_service_interface.go
@@ -0,0 +1,23 @@
+package service
+
+import (
+	"github.com/HarsaEdu/harsa-api/internal/app/submission/repository"
+	"github.com/HarsaEdu/harsa-api/internal/model/web"
+	"github.com/go-playground/validator"
+	"github.com/labstack/echo/v4"
+)
+
+type SubmissionService interface {
+	Create(ctx echo.Context, request web.SubmissionRequest) error
+	Update(ctx echo.Context, request *web.SubmissionUpdateRequest, submissionId int) error
+	Delete(id int) error
+}
+
+type SubmissionServiceImpl struct {
+	SubmissionRepository repository.SubmissionRepository
+	Validator            *validator.Validate
+}
+
+func NewSubmissionService(repository repository.SubmissionRepository, validate validator.Validate) SubmissionService {
+	return &SubmissionServiceImpl{SubmissionRepository: repository, Validator: &validate}
+}
