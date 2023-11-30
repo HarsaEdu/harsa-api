c8f71ff76961effb8935d978d524ed4f93f445a5
DimasAnugerahh <dimasanugrah883@gmail.com>
2023-12-01 02:37:41 +0800

    refactor(submission): update model

--- internal/model/domain/submissions.go
+++ internal/model/domain/submissions.go
@@ -7,11 +7,13 @@ import (
 )
 
 type Submissions struct {
-	ID          uint           `gorm:"type:int;primarykey" json:"id"`
-	ModuleID    uint           `json:"module_id"`
-	Title       string         `gorm:"type:varchar(225)" json:"title"`
-	Description string         `gorm:"type:text" json:"description"`
-	CreatedAt   time.Time      `gorm:"autoCreateTime" json:"created_at"`
-	UpdatedAt   time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
-	DeletedAt   gorm.DeletedAt `json:"deleted_at"`
+	ID                uint                `gorm:"type:int;primarykey" json:"id"`
+	ModuleID          uint                `gorm:"type:int" json:"module_id"`
+	UserId            uint                `json:"user_id"`
+	Title             string              `gorm:"type:varchar(225)" json:"title"`
+	Description       string              `gorm:"type:text" json:"description"`
+	SubmissionsAnswer []SubmissionsAnswer `gorm:"foreignKey:SubsmissionsID" json:"submissions_answer"`
+	CreatedAt         time.Time           `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt         time.Time           `gorm:"autoUpdateTime" json:"updated_at"`
+	DeletedAt         gorm.DeletedAt      `json:"deleted_at"`
 }
