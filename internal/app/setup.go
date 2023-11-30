4e8a16a6d9b854187ae709d36f1a5aa010baaf9f
AsZaychik <aszaychik@gmail.com>
2023-11-30 07:03:27 +0700

    feat(payment): serve web payment

--- internal/app/setup.go
+++ internal/app/setup.go
@@ -56,6 +56,7 @@ func InitApp(db *gorm.DB, validate *validator.Validate, cloudinary cloudinary.Cl
 	questionsRoutes.QuestionsWeb(coursesGroup)
 	optionsRoutes.OptionsWeb(coursesGroup)
 	feedbackRoutes.FeedbackWeb(apiGroupWeb)
+	paymentRoutes.PaymentWeb(apiGroupWeb)
 
 	apiGroupMobile := e.Group("mobile")
 	authRoutes.AuthMobile(apiGroupMobile)
