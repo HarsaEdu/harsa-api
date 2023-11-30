ca6fa24633cac94113e0ce9bac16a493d24f44c8
AsZaychik <aszaychik@gmail.com>
2023-11-30 08:05:20 +0700

    feat(payment): add payment history domain

---
+++ internal/model/domain/payment.go
@@ -0,0 +1,24 @@
+package domain
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type PaymentHistory struct {
+	ID          string         `gorm:"type:varchar(255);primarykey" json:"id"`
+	UserId      uint           `gorm:"type:int" json:"user_id"`
+	ItemId      uint           `gorm:"type:int" json:"item_id"`
+	Status      string         `gorm:"type:varchar(255)" json:"status"`
+	Method      string         `gorm:"type:varchar(255)" json:"method"`
+	GrossAmount string         `gorm:"type:string" json:"gross_amount"`
+	BankName    string         `gorm:"type:varchar(255)" json:"bank_name"`
+	VaNumber    string         `gorm:"type:varchar(255)" json:"va_number"`
+	CreatedAt   time.Time      `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt   time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
+	DeletedAt   gorm.DeletedAt `gorm:"index" json:"delete_at"`
+	ExpiredAt   time.Time      `gorm:"type:date" json:"expired_at"`
+	User        User           `gorm:"foreignKey:UserId;references:ID" json:"user"`
+	Item        SubsPlan       `gorm:"foreignKey:ItemId;references:ID" json:"item"`
+}
