26f79dd4df0c43a960796172c63f60dc0f3ca330
AsZaychik <aszaychik@gmail.com>
2023-11-30 10:14:38 +0700

    refactor(payment): move conversion req to res

---
+++ internal/pkg/conversion/response/payment.go
@@ -0,0 +1,30 @@
+package conversion
+
+import (
+	"github.com/HarsaEdu/harsa-api/internal/model/domain"
+	"github.com/HarsaEdu/harsa-api/internal/model/web"
+)
+
+func PaymentHistoryDomainToPaymentHistoryResponse(paymentHistory *domain.PaymentHistory) *web.GetPaymentResponse {
+	return &web.GetPaymentResponse{
+		ID:          paymentHistory.ID,
+		UserId:      paymentHistory.UserId,
+		ItemId:      paymentHistory.ItemId,
+		Status:      paymentHistory.Status,
+		Method:      paymentHistory.Method,
+		GrossAmount: paymentHistory.GrossAmount,
+		BankName:    paymentHistory.BankName,
+		VaNumber:    paymentHistory.VaNumber,
+		CreatedAt:   paymentHistory.CreatedAt,
+		UpdatedAt:   paymentHistory.UpdatedAt,
+		ExpiredAt:   paymentHistory.ExpiredAt,
+		Customer: web.PaymentCustomerResponse{
+			ID:   paymentHistory.User.ID,
+			Name: paymentHistory.User.UserProfile.FirstName + " " + paymentHistory.User.UserProfile.LastName,
+		},
+		Item: web.PaymentItemResponse{
+			ID:   paymentHistory.Item.ID,
+			Name: paymentHistory.Item.Title,
+		},
+	}
+}
