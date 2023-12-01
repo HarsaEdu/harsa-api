99ad01272945497fcbf8972b4bd5c24f24da0da5
DimasAnugerahh <dimasanugrah883@gmail.com>
2023-12-01 22:38:21 +0800

    refactor(submission): add end point

--- internal/app/submission/routes/submission_routes.go
+++ internal/app/submission/routes/submission_routes.go
@@ -9,9 +9,14 @@ func (subsPlanRoutes *SubmissionRoutesImpl) SubmissionWeb(apiGroup *echo.Group) 
 	submissionsGroup := apiGroup.Group("/modules/:moduleId/submissions")
 
 	submissionsGroup.POST("", subsPlanRoutes.submissionHandler.Create, middleware.InstructorMiddleware)
+	submissionsGroup.GET("", subsPlanRoutes.submissionHandler.GetAllWeb, middleware.InstructorMiddleware)
 	submissionsGroup.PUT("/:id", subsPlanRoutes.submissionHandler.Update, middleware.InstructorMiddleware)
 	submissionsGroup.DELETE("/:id", subsPlanRoutes.submissionHandler.Delete, middleware.InstructorMiddleware)
-
 }
 
-func (subsPlanRoutes *SubmissionRoutesImpl) SubmissionMobile(apiGroup *echo.Group) {}
+func (subsPlanRoutes *SubmissionRoutesImpl) SubmissionMobile(apiGroup *echo.Group) {
+	submissionsGroup := apiGroup.Group("/modules/:moduleId/submissions")
+
+	submissionsGroup.GET("", subsPlanRoutes.submissionHandler.GetAllMobile, middleware.InstructorMiddleware)
+
+}
