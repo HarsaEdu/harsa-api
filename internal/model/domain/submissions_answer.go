760ea6d8236b1c13318416ce7f220afe3ace9f9e
DimasAnugerahh <dimasanugrah883@gmail.com>
2023-12-01 22:35:31 +0800

    refactor(submission): update model

--- internal/model/domain/submissions_answer.go
+++ internal/model/domain/submissions_answer.go
@@ -14,6 +14,7 @@ type SubmissionsAnswer struct {
 	ID             uint   `gorm:"type:int;primarykey" json:"id"`
 	SubsmissionsID uint   `json:"subsmission_id" `
 	Content        string `gorm:"type:text" json:"content"`
+	Submited_url   string `gorm:"type:text" json:"submited_url"`
 	Status         Status `json:"status" gorm:"type:varchar(255)"`
 	Feedback       string `json:"feedback"`
 }
