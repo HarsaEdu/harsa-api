924bb2e281dde3f21738841deecce08d46e947d7
DimasAnugerahh <dimasanugrah883@gmail.com>
2023-12-03 03:34:45 +0800

    feat(submissionAns): add routes interface

---
+++ internal/app/submission_answer/routes/submission_answer_routes_interface.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"github.com/HarsaEdu/harsa-api/internal/app/submission_answer/handler"
+	"github.com/labstack/echo/v4"
+)
+
+type SubmissionAnswerRoutes interface {
+	SubmissionAnswerWeb(apiGroup *echo.Group)
+	// SubmissionAnswerMobile(apiGroup *echo.Group)
+}
+
+type SubmissionAnswerRoutesImpl struct {
+	SubmissionAnswerHandler handler.SubmissionAnswerHandler
+}
+
+func NewSubmissionAnswerRoutes(submissionAnswerHandler handler.SubmissionAnswerHandler) SubmissionAnswerRoutes {
+	return &SubmissionAnswerRoutesImpl{
+		SubmissionAnswerHandler: submissionAnswerHandler,
+	}
+}
