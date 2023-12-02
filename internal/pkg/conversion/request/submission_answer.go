cdf0f70d6944643989fb4f1f5944a0810eeedbe9
DimasAnugerahh <dimasanugrah883@gmail.com>
2023-12-03 03:38:05 +0800

    feat(submissionAns): add conversion

---
+++ internal/pkg/conversion/request/submission_answer.go
@@ -0,0 +1,10 @@
+package conversion
+
+import (
+	"github.com/HarsaEdu/harsa-api/internal/model/domain"
+	"github.com/HarsaEdu/harsa-api/internal/model/web"
+)
+
+func ConvertSubmissionAnswerRequestToSubmissionAnswerDomain(request *web.SubmissionAnswerRequest) *domain.SubmissionAnswer {
+	return &domain.SubmissionAnswer{SubmittedUrl: request.SubmittedUrl, Status: request.Status}
+}
