a4e94d98ad4dcd8951a04ef3b565dcd827d20a3a
yusufUcps <yusuffy2212@gmail.com>
2023-12-04 17:53:51 +0700

    refactor(mysql): duplicate database

--- internal/infrastructure/database/mysql.go
+++ internal/infrastructure/database/mysql.go
@@ -37,7 +37,6 @@ func migration(db *gorm.DB) {
 		&domain.Module{},
 		&domain.SubModule{},
 		&domain.Submissions{},
-		&domain.SubmissionsAnswer{},
 		&domain.Quizzes{},
 		&domain.Questions{},
 		&domain.Options{},
--- internal/model/domain/course_tracking.go
+++ internal/model/domain/course_tracking.go
@@ -43,6 +43,7 @@ const (
 	StatusSubmissionAnswerAccepted StatusSubmissionAnswer = "accepted"
 	StatusSubmissionAnswerPending  StatusSubmissionAnswer = "pending"
 	StatusSubmissionAnswerRejected StatusSubmissionAnswer = "rejected"
+	StatusSubmissionAnswerSubmitted StatusSubmissionAnswer = "submitted"
 	StatusSubmissionAnswerNotSubmitted StatusSubmissionAnswer = "not submitted"
 )
 
