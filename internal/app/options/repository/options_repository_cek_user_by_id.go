3c63b380583ab974a24db2a36f110d3d0aceffda
yusufUcps <yusuffy2212@gmail.com>
2023-12-06 00:09:45 +0700

    refactor(quizzes): update cek user id fix

--- internal/app/options/repository/options_repository_cek_user_by_id.go
+++ internal/app/options/repository/options_repository_cek_user_by_id.go
@@ -7,49 +7,47 @@ import (
 )
 
 func (repositoryOptions *OptionsRepositoryImpl) CekIdFromOption(userId uint, optionId uint, role string) (*domain.Options, error) {
-	tx := repositoryOptions.DB.Begin()
 
-	var option domain.Options
-	if err := tx.First(&option, optionId).Error; err != nil {
-		tx.Rollback()
+	var option = domain.Options{}
+
+	if err := repositoryOptions.DB.First(&option, optionId).Error; err != nil {
+		return nil, err
+	}
+	
+	var quizID uint
+
+	if err := repositoryOptions.DB.Model(&domain.Questions{}).Where("id = ?", option.QuestionID).Select("section_id").Scan(&quizID).Error; err != nil {
+		return nil, err
+	}
+	
+	var moduleID uint
+
+	if err := repositoryOptions.DB.Model(&domain.Quizzes{}).Where("id = ?", quizID).Select("section_id").Scan(&moduleID).Error; err != nil {
 		return nil, err
 	}
 
-	getScannedValue := func(model interface{}, condition string, value interface{}, dest interface{}) error {
-		if err := tx.Model(model).Where(condition, value).Select(dest).Scan(dest).Error; err != nil {
-			tx.Rollback()
-			return err
-		}
-		return nil
-	}
+	var sectionID uint
 
-	var userID, quizID, moduleID, sectionID, courseID uint
-
-	if err := getScannedValue(&domain.Questions{}, "id = ?", option.QuestionID, &quizID); err != nil {
+	if err := repositoryOptions.DB.Model(&domain.Module{}).Where("id = ?", moduleID).Select("section_id").Scan(&sectionID).Error; err != nil {
 		return nil, err
 	}
 
-	if err := getScannedValue(&domain.Quizzes{}, "id = ?", quizID, &moduleID); err != nil {
+	var courseID uint
+
+	if err := repositoryOptions.DB.Model(&domain.Section{}).Where("id = ?", sectionID).Select("course_id").Scan(&courseID).Error; err != nil {
 		return nil, err
 	}
+	
+	var userID uint
 
-	if err := getScannedValue(&domain.Module{}, "id = ?", moduleID, &sectionID); err != nil {
+	if err := repositoryOptions.DB.Model(&domain.Course{}).Where("id = ?", courseID).Select("user_id").Scan(&userID).Error; err != nil {
 		return nil, err
 	}
-
-	if err := getScannedValue(&domain.Section{}, "id = ?", sectionID, &courseID); err != nil {
-		return nil, err
-	}
-
-	if err := getScannedValue(&domain.Course{}, "id = ?", courseID, &userID); err != nil {
-		return nil, err
-	}
-
-	tx.Commit()
 
 	if userID != userId && role != "admin" {
 		return nil, fmt.Errorf("unauthorized")
 	}
+	
 
 	return &option, nil
 }
