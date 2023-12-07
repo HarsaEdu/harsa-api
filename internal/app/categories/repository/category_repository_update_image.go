7d52a5b490a7b826dad27c6fb5f8f4d43b72c9ca
DimasAnugerahh <dimasanugrah883@gmail.com>
2023-12-07 15:09:39 +0800

    refactor(category): add null validation

--- internal/app/categories/repository/category_repository_update_image.go
+++ internal/app/categories/repository/category_repository_update_image.go
@@ -4,8 +4,22 @@ import (
 	"github.com/HarsaEdu/harsa-api/internal/model/domain"
 )
 
-func (categoryRepository *CategoryRepositoryImpl) UpdateImage(imageUrl *domain.Category, id int) error {
-	result := categoryRepository.DB.Model(&domain.Category{}).Where("id=?", id).Update("image_url", imageUrl.Image_url)
+func (categoryRepository *CategoryRepositoryImpl) UpdateImage(imageUrl string, icon string, id int) error {
+
+	updateField := map[string]any{}
+
+	if imageUrl != "" {
+		updateField["image_url"] = imageUrl
+	}
+
+	if icon != "" {
+		updateField["icon"] = icon
+	}
+
+	result := categoryRepository.DB.Model(&domain.Category{}).
+		Where("id=?", id).
+		Updates(updateField)
+
 	if result.Error != nil {
 		return result.Error
 	}
