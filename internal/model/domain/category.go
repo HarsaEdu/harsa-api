3497bd25e7facf06500f05404f2929a162225a7d
DimasAnugerahh <dimasanugrah883@gmail.com>
2023-12-07 16:05:07 +0800

    refactor(subsPlan): update default value

--- internal/model/domain/category.go
+++ internal/model/domain/category.go
@@ -10,8 +10,8 @@ type Category struct {
 	ID          uint           `gorm:"type:int;primarykey" json:"id"`
 	Name        string         `gorm:"type:varchar(255)" json:"name"`
 	Description string         `gorm:"type:text" json:"description"`
-	Image_url   string         `gorm:"type:varchar(255);default:'https://res.cloudinary.com/dydgjkfgs/image/upload/v1701933791/harsa/categories/hsjfi05cx3pcvaaepglk.png'" json:"image"`
-	Icon        string         `gorm:"type:varchar(255);default:'https://res.cloudinary.com/dydgjkfgs/image/upload/v1701933791/harsa/categories/hsjfi05cx3pcvaaepglk.png'" json:"icon"`
+	Image_url   string         `gorm:"type:varchar(255);default:'https://api.lorem.space/image/dashboard?w=200&h=200'" json:"image"`
+	Icon        string         `gorm:"type:varchar(255);default:'https://res.cloudinary.com/dydgjkfgs/image/upload/v1701935601/harsa/categories/dxkcy2escqi2ugrrndvo.png'" json:"icon"`
 	CreatedAt   time.Time      `gorm:"autoCreateTime" json:"created_at"`
 	UpdatedAt   time.Time      `gorm:"autoUpdateTime" json:"updated_at"`
 	DeleteAt    gorm.DeletedAt `json:"delete_at"`
