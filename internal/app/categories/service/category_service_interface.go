18c3d1391e50335c64bd5d96ea3c2722accb610e
DimasAnugerahh <dimasanugrah883@gmail.com>
2023-12-07 15:10:07 +0800

    refactor(category):add parameter

--- internal/app/categories/service/category_service_interface.go
+++ internal/app/categories/service/category_service_interface.go
@@ -12,7 +12,7 @@ import (
 type CategoryService interface {
 	Create(ctx echo.Context, category web.CategoryCreateRequest) error
 	Update(category web.CategoryUpdateRequest, id int) error
-	UploadImage(ctx echo.Context, category *web.CategoryUploadImageRequest, id int) error
+	UploadImage(ctx echo.Context, request *web.CategoryUploadImageRequest, id int, isIconExist, isImageExist bool) error
 	FindById(id int) (*domain.Category, error)
 	GetAll(offset, limit int, search string) ([]domain.Category, *web.Pagination, error)
 	Delete(id int) error
