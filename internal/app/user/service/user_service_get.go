2740c6bd40a17491cadbfd1df72ec6a9dbf3c502
yusufUcps <yusuffy2212@gmail.com>
2023-12-10 00:06:39 +0700

    refactor(user): update get all with role id

--- internal/app/user/service/user_service_get.go
+++ internal/app/user/service/user_service_get.go
@@ -8,8 +8,8 @@ import (
 	conversion "github.com/HarsaEdu/harsa-api/internal/pkg/conversion/response"
 )
 
-func (userService *UserServiceImpl) UserGetAll(offset int, limit int, search string) ([]domain.UserEntity, *web.Pagination, error) {
-	users, total, err := userService.UserRepository.UserGetAll(offset, limit, search)
+func (userService *UserServiceImpl) UserGetAll(offset int, limit int, search string, roleId int) ([]domain.UserEntity, *web.Pagination, error) {
+	users, total, err := userService.UserRepository.UserGetAll(offset, limit, search, roleId)
 	if len(search) > 0 && total <= 0 {
 		return nil, nil, fmt.Errorf("users not found")
 	}
