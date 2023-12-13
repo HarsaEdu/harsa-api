b06212f712523c69893c55719aeb78ae525e6de8
yusufUcps <yusuffy2212@gmail.com>
2023-12-13 19:13:24 +0700

    feat(register): add free subscription in register

--- internal/app/user/repository/user_repository_create.go
+++ internal/app/user/repository/user_repository_create.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"time"
+
 	"github.com/HarsaEdu/harsa-api/internal/model/domain"
 )
 
@@ -12,6 +14,46 @@ func (userRepository *UserRepositoryImpl) UserCreate(user *domain.User) (*domain
 	if result.Error != nil {
 		return nil, result.Error
 	}
+
+	return user, nil
+}
+
+
+func (userRepository *UserRepositoryImpl) UserCreateMobile(user *domain.User) (*domain.User, error) {
+	tx := userRepository.DB.Begin()
+
+	defer func() {
+		if r := recover(); r != nil {
+			
+			tx.Rollback()
+		}
+	}()
+	
+	result := tx.Create(&user)
+	
+	if result.Error != nil {
+		
+		tx.Rollback()
+		return nil, result.Error
+	}
+
+	subscription := domain.Subscription{
+		StartDate: time.Now(),
+		UserID:    user.ID,
+	}
+
+	endDate := subscription.StartDate.Add(time.Hour * 24 * 7)
+	subscription.EndDate = endDate.UTC()
+	resultSubscribe := tx.Create(&subscription)
+	
+	if resultSubscribe.Error != nil {
+		
+		tx.Rollback()
+		return nil, resultSubscribe.Error
+	}
+
+	
+	tx.Commit()
 
 	return user, nil
 }
