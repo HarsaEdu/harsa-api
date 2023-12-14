77e22eec8f460ee74706f3d3f466f8d0868022e6
yusufUcps <yusuffy2212@gmail.com>
2023-12-14 21:00:07 +0700

    refactor(profile): remove validation update profile

--- internal/model/web/profile_request.go
+++ internal/model/web/profile_request.go
@@ -11,11 +11,11 @@ type UpdateProfileRequest struct {
 	FirstName   string    `gorm:"type:varchar(50)" json:"first_name" form:"first_name" validate:"required"`
 	LastName    string    `gorm:"type:varchar(50)" json:"last_name" form:"last_name" validate:"required"`
 	DateBirth   time.Time `gorm:"type:date" json:"date_birth" form:"date_birth" validate:"required"`
-	Bio         string    `gorm:"type:varchar(255)" json:"bio" form:"bio" validate:"required"`
+	Bio         string    `gorm:"type:varchar(255)" json:"bio" form:"bio"`
 	Gender      domain.Gender    `gorm:"type:enum('f','m')" json:"gender" form:"gender" validate:"required"`
 	PhoneNumber string    `gorm:"type:varchar(20)" json:"phone_number" form:"phone_number" validate:"required"`
 	City        string    `gorm:"type:varchar(20)" json:"city" form:"city" validate:"required"`
-	Address     string    `gorm:"type:varchar(255)" json:"address" form:"address" validate:"required"`
+	Address     string    `gorm:"type:varchar(255)" json:"address" form:"address"`
 	Job         string    `gorm:"type:varchar(20)" json:"job" form:"job" validate:"required"`
 }
 
