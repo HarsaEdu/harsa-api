3cf7250292cc355a1217c9c6070d8a5c85e31e8a
DimasAnugerahh <dimasanugrah883@gmail.com>
2023-12-15 01:00:24 +0800

    refactor(categories): update middleware

--- internal/app/categories/routes/category_routes.go
+++ internal/app/categories/routes/category_routes.go
@@ -10,7 +10,7 @@ func (categoryRoutes *CategoryRoutesImpl) CategoryWeb(apiGroup *echo.Group) {
 
 	categoriesGroup.POST("", categoryRoutes.CategoryHandler.Create, middleware.AdminMiddleware)
 	categoriesGroup.GET("", categoryRoutes.CategoryHandler.GetAll, middleware.InstructorMiddleware)
-	categoriesGroup.PUT("/:id", categoryRoutes.CategoryHandler.Update, middleware.InstructorMiddleware)
+	categoriesGroup.PUT("/:id", categoryRoutes.CategoryHandler.Update, middleware.AdminMiddleware)
 	categoriesGroup.PATCH("/:id", categoryRoutes.CategoryHandler.UploadImage, middleware.AdminMiddleware)
 	categoriesGroup.DELETE("/:id", categoryRoutes.CategoryHandler.Delete, middleware.AdminMiddleware)
 	categoriesGroup.GET("/:id", categoryRoutes.CategoryHandler.FindById, middleware.AdminMiddleware)
