1d4618d619aac9846452c34de4caea0c9830b120
yusufUcps <yusuffy2212@gmail.com>
2023-12-15 19:01:59 +0700

    refactor(submission_answer): get by id web

--- internal/app/submission_answer/repository/submission_answer_repository_find_by_id.go
+++ internal/app/submission_answer/repository/submission_answer_repository_find_by_id.go
@@ -1,10 +1,26 @@
 package repository
 
-import "github.com/HarsaEdu/harsa-api/internal/model/domain"
+import (
+	"github.com/HarsaEdu/harsa-api/internal/model/domain"
+	"github.com/HarsaEdu/harsa-api/internal/model/web"
+)
 
 func (submissionAnswerRepository *SubmissionAnswerRepositoryImpl) FindById(id int) (*domain.SubmissionAnswer, error) {
 	submissionAnswer := &domain.SubmissionAnswer{}
 	result := submissionAnswerRepository.DB.Where("id=?", id).First(submissionAnswer)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return submissionAnswer, nil
+}
+
+func (submissionAnswerRepository *SubmissionAnswerRepositoryImpl) FindByIdWeb(id int) (*web.SubmissionAnswerResponseWebById, error) {
+	submissionAnswer := &web.SubmissionAnswerResponseWebById{}
+	result := submissionAnswerRepository.DB.Model(&domain.SubmissionAnswer{}).Where("submission_answers.id=?", id).Select("submission_answers.feedback as feedback, submission_answers.id as id, submission_answers.user_id as student_id, submission_answers.status as status,submission_answers.submitted_url as submitted_url, submissions.content as description, concat(user_profiles.first_name, ' ', user_profiles.last_name) as student_name").
+	Joins("LEFT JOIN submissions ON submissions.id = submission_answers.submission_id").
+	Joins("LEFT JOIN user_profiles ON user_profiles.id = submission_answers.user_id").
+	Find(&submissionAnswer)
 	if result.Error != nil {
 		return nil, result.Error
 	}
